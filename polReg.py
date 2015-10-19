ad205f9720b0128733e7dc07376e173389500892
Patrick Huston <patrickhuston1@gmail.com>
2015-10-19 15:06:52 -0400

    Implements calcTotalError function

--- polReg.py
+++ polReg.py
@@ -2,6 +2,7 @@ from __future__ import division
 import numpy as np
 import sympy as sp
 import string
+import itertools
 
 points = [(1, 1), (1, 0), (2, 2), (12, 5432)]
 
@@ -26,6 +27,54 @@ def calcError(points, coeffVals):
 		totalError += (yVal - funcVal)**2
 
 	return totalError
+
+def calcTotalError(points, coeffMatrices):
+	'''
+	Calculates total error over entire domain for each set
+	of coefficient values
+	
+	Inputs:
+		coeffMatrices (list of np matrices): List of matrices
+		representing all possible combinations of coeff vals
+
+	Returns: 
+		errorMatrix (np matrix): Error at each given pointl		 	
+
+	'''
+	errorMatrix = np.zeros([coeffMatrices[0].shape[i] for i in range(len(coeffMatrices.shape[0]))])
+
+	ranges = [range(coeffMatrix.shape[i]) for i in range(len(coeffMatrix.shape)) for coeffMatrix in coeffMatrices]
+
+	domain = itertools.product(*ranges)
+
+	for location in domain:
+		coeffVals = [coeffMatrix[location] for coeffMatrix in coeffMatrices]
+		errorAtLoc = calcError(points, coeffVals)
+		errorMatrix[location] = errorAtLoc
+
+	return errorMatrix
+
+
+def indexByTuple(matrix, index):
+	'''
+	Indexes matrix by location represented as tuples
+	i.e. indexByTuple(matrix, (1,2,3)) = matrix[1][2][3]
+
+	Inputs:
+		matrix (np array): Matrix to be indexed
+		index (tuple): Represents location in matrix
+
+	Returns
+		val (int): Value at location 'index'
+
+	'''
+
+	val = matrix
+	for i in index:
+		val = val[i]
+
+	return val
+
 
 def calcDeriv(points, coeffVals, indexCoeff):
 	'''
