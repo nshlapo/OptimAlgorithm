050e9afb2f03c368b51ce5ea0c740b3630b9c2f1
nshlapo <nur.shlapobersky@students.olin.edu>
2015-10-20 22:46:47 -0400

    Created wrapper and finished gradient descent implementation

--- polReg.py
+++ polReg.py
@@ -3,109 +3,108 @@ import numpy as np
 import itertools
 
 
-points = [(1, 1), (1, 0), (2, 2), (12, 5432)]
-
-coeffVals = np.meshgrid(*[np.linspace(-100, 100, 2000) for i in range(degree)], sparse=True)
-# coeffVals = [1, 1, 1, 1]
 
 def calc_point_error(points, coeffVals):
-	'''
-	Calculates error by taking sum of error between each
-	point and the polynomial function
+    '''
+    Calculates error by taking sum of error between each
+    point and the polynomial function
 
-	Inputs:
-		points (list of tuples): Points to fit
-		coeffVals (list): Coefficients of polynomial function
-	Returns:
-		totalError (int): Sum of errors squared
-	'''
-	totalError = 0
-	for point in points:
-		xVal = point[0]
-		yVal = point[1]
-		funcVal = sum([coeff*(xVal**index) for index, coeff in enumerate(coeffVals)])
-		totalError += (yVal - funcVal)**2
+    Inputs:
+        points (list of tuples): Points to fit
+        coeffVals (list): Coefficients of polynomial function
+    Returns:
+        totalError (int): Sum of errors squared
+    '''
+    totalError = 0
+    for point in points:
+        xVal = point[0]
+        yVal = point[1]
+        funcVal = sum([coeff*(xVal**index) for index, coeff in enumerate(coeffVals)])
+        totalError += (yVal - funcVal)**2
 
-	return totalError
+    return totalError
 
 def calc_all_error(points, coeffMatrices):
-	'''
-	Calculates total error over entire domain for each set
-	of coefficient values
+    '''
+    Calculates total error over entire domain for each set
+    of coefficient values
 
-	Inputs:
-		coeffMatrices (list of np matrices): List of matrices
-		representing all possible combinations of coeff vals
+    Inputs:
+        coeffMatrices (list of np matrices): List of matrices
+        representing all possible combinations of coeff vals
 
-	Returns:
-		errorMatrix (np matrix): Error at each given pointl
+    Returns:
+        errorMatrix (np matrix): Error at each given pointl
 
-	'''
-	errorMatrix = np.zeros([coeffMatrices[0].shape[i] for i in range(len(coeffMatrices.shape[0]))])
+    '''
+    errorMatrix = np.zeros([coeffMatrices[0].shape[i] for i in range(len(coeffMatrices.shape[0]))])
 
-	ranges = [range(coeffMatrix.shape[i]) for i in range(len(coeffMatrix.shape)) for coeffMatrix in coeffMatrices]
+    ranges = [range(coeffMatrix.shape[i]) for i in range(len(coeffMatrix.shape)) for coeffMatrix in coeffMatrices]
 
-	domain = itertools.product(*ranges)
+    domain = itertools.product(*ranges)
 
-	for location in domain:
-		coeffVals = [coeffMatrix[location] for coeffMatrix in coeffMatrices]
-		errorAtLoc = calcError(points, coeffVals)
-		errorMatrix[location] = errorAtLoc
+    for location in domain:
+        coeffVals = [coeffMatrix[location] for coeffMatrix in coeffMatrices]
+        errorAtLoc = calcError(points, coeffVals)
+        errorMatrix[location] = errorAtLoc
 
-	return errorMatrix
+    return errorMatrix
 
 
 def indexByTuple(matrix, index):
-	'''
-	Indexes matrix by location represented as tuples
-	i.e. indexByTuple(matrix, (1,2,3)) = matrix[1][2][3]
+    '''
+    Indexes matrix by location represented as tuples
+    i.e. indexByTuple(matrix, (1,2,3)) = matrix[1][2][3]
 
-	Inputs:
-		matrix (np array): Matrix to be indexed
-		index (tuple): Represents location in matrix
+    Inputs:
+        matrix (np array): Matrix to be indexed
+        index (tuple): Represents location in matrix
 
-	Returns
-		val (int): Value at location 'index'
+    Returns
+        val (int): Value at location 'index'
 
-	'''
+    '''
 
-	val = matrix
-	for i in index:
-		val = val[i]
+    val = matrix
+    for i in index:
+        val = val[i]
 
-	return val
+    return val
 
 
 def calc_gradient(points, coeffVals, indexCoeff):
-	'''
-	Calculates derivative of function with respect to given
-	coefficient index at each given error function location
+    '''
+    Calculates derivative of function with respect to given
+    coefficient index at each given error function location
 
-	Inputs:
-		points (list of tuples): Points to fit
-		coeffVals (list): Coefficients of polynomial function
-		indexCoeff (int): Term of function for which derivative
-		is taken respect to
-	Returns:
-		finalDeriv (int): Value of derivative of function with
-		respect to the given index
-	'''
-	for point in points:
-		xVal = point[0]
-		yVal = point[1]
-		xDepDeriv = sum([coeff*(xVal**(index+indexCoeff)) for index, coeff in enumerate(coeffVals)])
-		deriv = yVal*(xVal**(indexCoeff)) - xDepDeriv
+    Inputs:
+        points (list of tuples): Points to fit
+        coeffVals (list): Coefficients of polynomial function
+        indexCoeff (int): Term of function for which derivative
+        is taken respect to
+    Returns:
+        finalDeriv (int): Value of derivative of function with
+        respect to the given index
+    '''
+    for point in points:
+        xVal = point[0]
+        yVal = point[1]
+        xDepDeriv = sum([coeff*(xVal**(index+indexCoeff)) for index, coeff in enumerate(coeffVals)])
+        deriv = yVal*(xVal**(indexCoeff)) - xDepDeriv
 
-	finalDeriv = -2*deriv
-	return finalDeriv
+    finalDeriv = -2*deriv
+    return finalDeriv
 
 def calc_all_gradient(points, coeffMatrices):
 
+def pol_reg(points):
+    points = [(1, 1), (1, 0), (2, 2), (12, 5432)]
+    degree = 1
+    while some condition:
+        coeffVals = np.meshgrid(*[np.linspace(-100, 100, 2000) for i in range(degree)], sparse=True)
+        degree += 1
 
-# In final function, iterate over degrees until satisfactory fit is reached
-degree = 22
-
-def gradientDescent(coeffs, degree):
+def gradientDescent(points, coeffVals, coeffMatrices, degree):
     ''' Performs a gradient descent using coeffs as the domain and returns the
         location of the minimum (in coefficient space), and the number of
         iterations to reach the minumum.
@@ -115,9 +114,8 @@ def gradientDescent(coeffs, degree):
     '''
 
     # initialize variables and arrays we'll need for gr dsc
-    grad = calcDeriv(points, coeffVals, indexCoeff)
     iCoeffs = numpy.zeros(degree)
-    iz = func(coeffs)
+    iz = calc_error(points, coeffVals)
     lambdas = np.logspace(-8, 1, 50)
     it = 0
     grad = numpy.ones(degree)
@@ -125,10 +123,10 @@ def gradientDescent(coeffs, degree):
     # perform the gr dsc
     while (np.linalg.norm(grad) > .00001):
         it  = it + 1
-        grad = gradient(coeffs)
+        grad = calc_all_gradient(points, coeffMatrices)
         optiLambda = optimalLambda(iCoeffs, grad, lamdbas)
         fCoeffs = iCoeffs - optiLambda*grad;
-        fz = func(fCoeffs);
+        fz = calc_all_error(points, coeffMatrices);
         iCoeffs = fCoeffs;
         iz = fz;
 
@@ -143,8 +141,8 @@ def optimalLambda(iCoeffs, grad, lambdas):
     '''
 
     pCoeffs = iCoeffs - lambdas*grad
-    pZ = func(pCoeffs)
-    ind = Zp.argmax(axis=0)
+    pZ = calc_error(points, pCoeffs)
+    ind = pZ.argmax(axis=0)
     optiLambda = lambdas[ind];
 
     return optiLamdba
